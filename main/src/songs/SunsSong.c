5c7a52eba93763073c1e964a4b0105fcecd7ca3d
Jose Rodriguez <joehacksalot@ironwoodcyber.com>
2024-06-24 21:42:58 -0400

    fix: update to sun song

--- main/src/songs/SunsSong.c
+++ main/src/songs/SunsSong.c
@@ -3,28 +3,28 @@
 
 const SongNotes SunsSong = {
   "Sun's Song",
-  100,  // Tempo
-  14,   // Number of Notes
+  150,  // Tempo
+  16,   // Number of Notes
   {
     {NOTE_A3, NOTE_TYPE_EIGHTH, 0},
     {NOTE_F3, NOTE_TYPE_EIGHTH, 0},
-    {NOTE_D4, NOTE_TYPE_QUARTER_DOT, 0},
+    {NOTE_D4, NOTE_TYPE_QUARTER, 0},
     {NOTE_REST, NOTE_TYPE_EIGHTH, 0},
-
-    // --
-
     {NOTE_A3, NOTE_TYPE_EIGHTH, 0},
     {NOTE_F3, NOTE_TYPE_EIGHTH, 0},
-    {NOTE_D4, NOTE_TYPE_QUARTER_DOT, 0},
+    {NOTE_D4, NOTE_TYPE_QUARTER, 0},
     {NOTE_REST, NOTE_TYPE_EIGHTH, 0},
 
     // --
 
+    {NOTE_A3, NOTE_TYPE_SIXTEENTH, 0},
+    {NOTE_B3, NOTE_TYPE_SIXTEENTH, 0},
     {NOTE_C4, NOTE_TYPE_SIXTEENTH, 0},
     {NOTE_D4, NOTE_TYPE_SIXTEENTH, 0},
     {NOTE_E4, NOTE_TYPE_SIXTEENTH, 0},
     {NOTE_F4, NOTE_TYPE_SIXTEENTH, 0},
-    {NOTE_G4, NOTE_TYPE_HALF, 1},
+    {NOTE_G4, NOTE_TYPE_SIXTEENTH, 1},
+    {NOTE_G4, NOTE_TYPE_HALF_DOT, 1},
 
     // --
     {NOTE_G4, NOTE_TYPE_HALF_DOT, 0},
