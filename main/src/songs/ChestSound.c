f7430e09be20499f31a35d2f272584f1dcf281f7
Ethan Puchaty <ethan.puchaty@ironwoodcyber.com>
2024-07-02 16:41:36 -0600

    fix: add chest sound

--- main/src/songs/ChestSound.c
+++ main/src/songs/ChestSound.c
@@ -3,12 +3,12 @@
 
 const SongNotes ChestSound = {
   "Chest Sound",
-  240,   // Tempo
+  140,   // Tempo
   4,   // Number of Notes
   {
-    {NOTE_G4, NOTE_TYPE_EIGHTH, 1},
-    {NOTE_AS5, NOTE_TYPE_EIGHTH, 1},
-    {NOTE_B5, NOTE_TYPE_EIGHTH, 1},
-    {NOTE_C5, NOTE_TYPE_QUARTER_DOT, 1}
+    {NOTE_G4, NOTE_TYPE_QUARTER_TRIPLET, 1},
+    {NOTE_AS4, NOTE_TYPE_QUARTER_TRIPLET, 1},
+    {NOTE_B4, NOTE_TYPE_QUARTER_TRIPLET, 1},
+    {NOTE_C5, NOTE_TYPE_QUARTER, 1}
   }
 };
