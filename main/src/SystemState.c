30a8118d8fcc1040794512e61c6b4e20c2009ce6
Cooper Wiegand <cooper.wiegand@ironwoodcyber.com>
2024-07-09 18:42:48 -0600

    fix: wifi refresh

--- main/src/SystemState.c
+++ main/src/SystemState.c
@@ -540,6 +540,7 @@ static void SystemState_BleNotificationHandler(void *pObj, esp_event_base_t even
             {
                 ESP_LOGW(TAG, "Failed to update user settings");
             }
+            WifiClient_Disconnect(&this->wifiClient);
             GameState_SendHeartBeat(&this->gameState, 0);
         }
         else
